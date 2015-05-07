fff23c966c004731419d3b1a24765f2fadbcf73b
Development Sector <contact@desctor.com>
2015-05-07 10:27:09 -0400

    remoteinfob

--- app/components/charts/barchart-potential.js
+++ app/components/charts/barchart-potential.js
@@ -4,6 +4,13 @@ export default Ember.Component.extend({
 
   didInsertElement: function () {
 
+    var firstCalendar = this.get('firstCalendar');
+    var secondCalendar = this.get('secondCalendar');
+
+    if (firstCalendar == null || secondCalendar == null) {
+      return;
+    }
+
     var chartId = this.get("chart-id");
     var ctx = document.getElementById(chartId).getContext("2d");
 
@@ -17,7 +24,21 @@ export default Ember.Component.extend({
           strokeColor: "rgba(220,220,220,0.8)",
           highlightFill: "rgba(220,220,220,0.75)",
           highlightStroke: "rgba(220,220,220,1)",
-          data: [65, 59, 80, 81, 56, 55, 40, 22, 14, 43, 23, 8]
+          data: [
+            firstCalendar.jan,
+            firstCalendar.feb,
+            firstCalendar.mar,
+            firstCalendar.apr,
+            firstCalendar.may,
+            firstCalendar.jun,
+            firstCalendar.jul,
+            firstCalendar.aug,
+            firstCalendar.sep,
+            firstCalendar.oct,
+            firstCalendar.nov,
+            firstCalendar.dec
+          ]
+
         },
         {
           label: "My Second dataset",
@@ -25,14 +46,26 @@ export default Ember.Component.extend({
           strokeColor: "rgba(151,187,205,0.8)",
           highlightFill: "rgba(151,187,205,0.75)",
           highlightStroke: "rgba(151,187,205,1)",
-          data: [28, 48, 40, 19, 86, 27, 90, 25, 50, 104, 22, 10]
+          data: [
+            secondCalendar.jan,
+            secondCalendar.feb,
+            secondCalendar.mar,
+            secondCalendar.apr,
+            secondCalendar.jun,
+            secondCalendar.jul,
+            secondCalendar.aug,
+            secondCalendar.sep,
+            secondCalendar.oct,
+            secondCalendar.nov,
+            secondCalendar.dec
+          ]
         }
       ]
     };
 
 
     if (this.get('on-modal') != null) {
-      $(this.get('on-modal')).on('shown.bs.modal', function (event) {
+      Ember.$(this.get('on-modal')).on('shown.bs.modal', function (event) {
         console.log('ctttxxx');
         var myBarChart = new Chart(ctx).Bar(data);
       });
