6863080597259408a5a161fb200feb5b33faa224
Marius Leustean <marius@softservlet.com>
2015-07-02 18:19:44 +0300

    improved menu items active state, swapped market profile and products pricing

--- app/views/setup.js
+++ app/views/setup.js
@@ -2,12 +2,4 @@ import Ember from 'ember';
 
 export default Ember.View.extend({
 
-  didInsertElement: function() {
-    var self = this;
-
-    this.$('li a').click(function() {
-      self.$('li').removeClass('active');
-      $(this).parents('li').addClass('active');
-    });
-  }
 });
